90489d59790cda8d9207232276d90a2aa5e60d58
alessandro <martinellialessandro04@gmail.com>
2022-11-09 08:19:47 +0100

    aa

--- biglietti/utils/ticket_price.php
+++ biglietti/utils/ticket_price.php
@@ -1,5 +1,7 @@
 <?php
 
+// filter_input va a prendere i nomi dei campi del form
+// ne prende i valori e ritorna il valore o false se non esiste
 $price = filter_input(INPUT_GET, 'price', FILTER_VALIDATE_FLOAT);
 $train = filter_input(INPUT_GET, 'train', FILTER_VALIDATE_FLOAT);
 
