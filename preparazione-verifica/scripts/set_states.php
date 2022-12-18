4fb56ddbbecdc5460729c64c3e4eb028c6e7eca3
alessandro <martinellialessandro04@gmail.com>
2022-12-18 11:02:10 +0100

    gui

--- preparazione-verifica/scripts/set_states.php
+++ preparazione-verifica/scripts/set_states.php
@@ -1,1 +1,17 @@
 <?php
+session_start();
+
+if (!isset($_SESSION['states'])) {
+    $states = array(
+        array("name" => "Italy", "age" => 18),
+        array("name" => "USA", "age" => 21),
+        array("name" => "France", "age" => 19),
+        array("name" => "Germany", "age" => 16),
+        array("name" => "Spain", "age" => 14),
+        array("name" => "UK", "age" => 32),
+        array("name" => "Russia", "age" => 2),
+        array("name" => "China", "age" => 20),
+    );
+
+    $_SESSION['states'] = $states;
+}
